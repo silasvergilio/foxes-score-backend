c892715aa00e8eb4b95eda23f2d318c44554b9ef
Silas Vergilio <silver@Silass-MacBook-Air.local>
2025-04-27 04:08:19 -0300

    fix/port

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.set('io', io);
 io.on("connection", (socket) => {
 });
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log("server running at http://localhost:3000");
 });
 
